d9a9f534e1dc8fb2b01d5c435794b9d7a05d2cb5
Enio <ebtronic15@gmail.com>
2019-05-03 20:23:47 +0200

    Initial commit

---
+++ app/src/main/java/com/blondi/cvapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.blondi.cvapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.profile_image_and_name.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
